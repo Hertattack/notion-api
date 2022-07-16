6aa005b9ebb290ca7003ced8527c697e52817d84
Marcel <marcel.schot@gmail.com>
2022-07-16 09:38:17 +0200

    Refactor input into own class

--- frontend/src/src/components/FreeQuery.tsx
+++ frontend/src/src/components/FreeQuery.tsx
@@ -1,15 +1,13 @@
 import React, {useState} from "react";
+import {QueryInput} from "./QueryInput";
 
 export const FreeQuery : React.FC = () => {
-    const [queryText, setQueryText] = useState('');
     const [previousQueries, setPreviousQueries] = useState<string[]>([]);
-    const [historyIndex, setHistoryIndex] = useState(0);
     const [queryResult, setQueryResult] = useState({});
 
-    function click() {
+    function performQuery(queryText : string) {
         if(!previousQueries.some(pq=>pq == queryText)){
             setPreviousQueries([...previousQueries, queryText]);
-            setHistoryIndex(previousQueries.length);
         }
 
         let uri = `https://localhost:7136/Query?query=${encodeURIComponent(queryText)}`;
@@ -20,33 +18,10 @@ export const FreeQuery : React.FC = () => {
             .catch( e => alert(e));
     }
 
-    function searchHistory(e: React.KeyboardEvent<HTMLInputElement>) {
-        let updatedHistoryIndex : number = -1;
-
-        if(e.key == 'ArrowUp' && historyIndex > 0){
-                updatedHistoryIndex = historyIndex - 1;
-        }else if(e.key == 'ArrowDown' && historyIndex < previousQueries.length - 1){
-            updatedHistoryIndex = historyIndex + 1;
-        }
-
-        if(updatedHistoryIndex >= 0){
-            setHistoryIndex(updatedHistoryIndex);
-            setQueryText(previousQueries[updatedHistoryIndex]);
-            (document.getElementById("queryText") as HTMLInputElement).value = queryText;
-        }
-    }
-
-    function handleChange(event : React.ChangeEvent<HTMLInputElement>) {
-        setQueryText(event.target.value);
-    }
-
     return <div>
-        <form>
-            <input type={"text"} size={100} id={"queryString"} value={queryText} onChange={handleChange} onKeyUp={e=>searchHistory(e)}/><button type={"button"} onClick={click}>Click me</button>
-        </form>
+        <QueryInput executeQueryCallback={performQuery} queryHistory={previousQueries}/>
         <br/>
         <div>
-            {historyIndex}
             <ol>
                 {previousQueries.map((pq)=>(
                     <li key={pq}>{pq}</li>
---
+++ frontend/src/src/components/QueryInput.tsx
@@ -0,0 +1,62 @@
+import React, {useRef, useState} from "react";
+
+interface QueryInputProps {
+    executeQueryCallback : (queryText : string) => void,
+    queryHistory : string[],
+}
+
+export const QueryInput : React.FC<QueryInputProps> = (props) => {
+    const [queryText, setQueryText] = useState('');
+    const  [selectedHistoryEntryIndex, setHistoryEntryIndex] = useState<number | null>(null);
+
+    const queryTextInputRef = useRef<HTMLInputElement | null>(null);
+
+    function handleClick() {
+        if(queryText.trim() == '')
+            return;
+
+        const {queryHistory} = props;
+        if(selectedHistoryEntryIndex && queryHistory[selectedHistoryEntryIndex] != queryText)
+            setHistoryEntryIndex(null);
+
+        props.executeQueryCallback(queryText);
+    }
+
+    function searchHistory(e: React.KeyboardEvent<HTMLInputElement>) {
+        const {queryHistory} = props;
+        let updatedHistoryEntryIndex = (selectedHistoryEntryIndex ? selectedHistoryEntryIndex : 0);
+
+        if(e.key == 'ArrowUp'){
+            updatedHistoryEntryIndex--;
+        }else if(e.key == 'ArrowDown'){
+            updatedHistoryEntryIndex++;
+        }
+
+        if(updatedHistoryEntryIndex != selectedHistoryEntryIndex
+            && updatedHistoryEntryIndex >= 0 && updatedHistoryEntryIndex < queryHistory.length){
+            setHistoryEntryIndex(updatedHistoryEntryIndex);
+            setQueryText(queryHistory[updatedHistoryEntryIndex]);
+
+            if(queryTextInputRef.current)
+                queryTextInputRef.current.value = queryText;
+        }
+    }
+
+    function handleChange(event : React.ChangeEvent<HTMLInputElement>) {
+        setQueryText(event.target.value);
+    }
+
+    return (
+      <form>
+          <input
+              ref={queryTextInputRef}
+              type={"text"}
+              size={100}
+              id={"queryString"}
+              value={queryText}
+              onChange={handleChange}
+              onKeyUp={e=>searchHistory(e)}/>
+          <button type={"button"} onClick={handleClick}>Execute</button>
+      </form>
+    );
+}
